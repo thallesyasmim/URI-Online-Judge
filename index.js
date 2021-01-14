fb76b60eaeea8ec38d8d3803d87b37119f4d24c8
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-01-14 10:59:31 -0300

    URI 1015 | Distância Entre Dois Pontos

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Lê o conteúdo do arquivo 'stdin' e armazena na variável input
 const input = require('fs').readFileSync('./dev/stdin', 'utf-8');
 // Dividindo o input num array à partir dos espaçamentos e convertendo as strings para numbers
-const [A, B] = input.split('\r\n').map(value => Number(value));
-// Valor absoluto de A - B
-const consumo = A / B;
-console.log(`${consumo.toFixed(3)} km/l`);
+const valores = input.split('\r\n') // Retorna cada linha do arquivo "stdin" numa posição de um Array
+const [ x1, y1 ] = valores[0].split(' ')
+const [ x2, y2 ] = valores[1].split(' ')
+// Fórmula para calcular há distância percorrida entre os dois pontos
+const distancia = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)); // sqrt() - Retorna a Raiz Quadrada de um número, pow() - Potenciação
+console.log(distancia.toFixed(4));
