8bfd17df57546e710beef4eee093082176e3829e
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-01-14 11:57:36 -0300

    URI 1017  | Gasto de Combustível

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // Lê o conteúdo do arquivo 'stdin' e armazena na variável input
 const input = require('fs').readFileSync('./dev/stdin', 'utf-8');
 // Dividindo o input num array à partir dos espaçamentos e convertendo as strings para numbers
-const valores = input.split('\r\n') // Retorna cada linha do arquivo "stdin" numa posição de um Array
-const [ x1, y1 ] = valores[0].split(' ')
-const [ x2, y2 ] = valores[1].split(' ')
-// Fórmula para calcular há distância percorrida entre os dois pontos
-const distancia = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)); // sqrt() - Retorna a Raiz Quadrada de um número, pow() - Potenciação
-console.log(distancia.toFixed(4));
+const [ tempo, velocidade ] = input.split('\r\n').map(value => Number(value)); // Retorna cada linha do arquivo "stdin" numa posição de um Array
+// Velocidade Média (Km/h) * Tempo da viagem / 12 Km/l = 1 litro = 12km
+const combustivelGasto = (tempo * velocidade) / 12; 
+console.log(combustivelGasto.toFixed(3));
